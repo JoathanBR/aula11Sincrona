dfc1df6fd50781e5569bca715c5b88161bf2059e
joão Hitallo <joaohitallo97@gmail.com>
2020-11-25 21:42:12 -0300

    aceita corno

--- funcoes.js
+++ funcoes.js
@@ -145,3 +145,121 @@ function ladosTriangulo(){
 
 var verificarLados = document.getElementById('verificarLados')
 verificarLados.addEventListener('click', ladosTriangulo)
+
+//----------
+function caixaEletronico() {
+    var valor = document.getElementById('valorSaque').value
+
+    let nota100 = 100
+    let nota50  = 50 
+    let nota10  = 10
+    
+
+    if ( valor >= nota100){
+        nota100 = valor / 100
+        valor = valor % 100;
+    }
+    if ( valor >= nota50){
+        nota50 = valor / 50
+        valor = valor % 50
+    }
+    if ( valor >= nota10){
+        nota10 = valor / 10
+        valor = valor % 10
+    }
+
+
+    if (valor >= 1) {
+        alert("Valor indiposnivel para as notas disponíveis (R$10, R$50, 100)" )
+        return
+    } else {
+        document.getElementById('mostrarNotasSaque').innerHTML = "<h3>" + "Notas de R$ 100: " + Math.trunc(nota100) + "<br>"
+        + "Notas de R$ 50: " + Math.trunc(nota50) + "<br>"
+        + "Notas de R$ 10: " + Math.trunc(nota10) + "<br>" + "</h3>"
+
+    }
+
+
+}
+
+var verificarNotasSaque = document.getElementById('verificarNotasSaque')
+verificarNotasSaque.addEventListener('click', caixaEletronico)
+
+function numDecrecente () {
+    var num = document.getElementById('numeroDecrecente').value
+
+    var listDecrecente = "Entre " + num + " e 1: "
+
+    for (num; num > 0; num--) {
+        listDecrecente += num + ", "
+        
+    }
+
+    document.getElementById('listaDecrecente').innerHTML = " <h3> " + listDecrecente + " </h3>"
+}
+
+var decrecer = document.getElementById('decrecer')
+decrecer.addEventListener('click', numDecrecente)
+
+function criacaoChinchilas () {
+    var numChinchilas = document.getElementById('numeroChinchilas').value
+    var anos = document.getElementById('nAnos').value
+
+    let result = ""
+
+    if (numChinchilas < 2){
+        alert("Numero de chinchilas deve ser maior que 2" )
+        return
+    }else {
+        for (var i = 1; i <=  anos; i++){
+            result += i + "º Ano:" + numChinchilas + " Chinchilas <br>"
+            numChinchilas = numChinchilas * 3
+        }
+    
+        document.getElementById('listaChinchilas').innerHTML=result
+    }
+}
+
+var criarChinchilas = document.getElementById('bttChinchilas')
+criarChinchilas.addEventListener('click', criacaoChinchilas)
+
+function numeroPerfeito () {
+    var num = document.getElementById('numeroP').value 
+
+    let result = "Divisores do " + num + ": 1, "
+    let soma = 1
+
+    for (var i=2; i < num; i++) {
+        if (num%i == 0){
+            soma +=  i
+            result += i + ", "
+        }
+    }
+    if (soma==num) {
+        document.getElementById('listaNumeroP').innerHTML= "<h3>" + result 
+        + "(Soma: " + soma + ") <br>" + num + " É um numero Perfeito </h3>"
+    } else {
+        document.getElementById('listaNumeroP').innerHTML= "<h3>" + result 
+        + "(Soma: " + soma + ") <br>" + num + " Não é um numero Perfeito </h3>"
+    }
+}
+
+var numPerfeito = document.getElementById('bttNumeroP')
+numPerfeito.addEventListener('click', numeroPerfeito)
+
+function criarEstrelas () {
+    var numLinha = document.getElementById('numeroLinhas').value
+    let estrela = '*'
+    let result = ""
+    let cont = 0
+
+    for (numLinha; numLinha > 0; numLinha--){
+        cont++
+        result += estrela.repeat(cont) + "<br>"
+    }
+
+    document.getElementById('listaEstrelas').innerHTML= result
+}
+
+var fabricaEstrelas = document.getElementById('bttEstrelas')
+fabricaEstrelas.addEventListener('click', criarEstrelas)
